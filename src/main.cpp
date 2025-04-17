0af1db5873de816ecff6c700946653683113286f
Kostasbuilts <kkir06@gmail.com>
2025-04-17 15:11:59 +0300

    big Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,236 @@
 #include <Arduino.h>
-#include <lcd_menu.cpp>
+#include<ADS1115_WE.h> 
+#include<Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define LCD_WIDTH   16
-#define LCD_HEIGHT  2   
-#define UP          6
-#define DOWN        7
-#define SELECT      8
+#define ADC_ADDRESS   0x48
+#define LCD_ADDRESS    0x27
+#define LCD_WIDTH        16
+#define LCD_HEIGHT       2   
+#define UP                       6
+#define DOWN                7
+#define SELECT                8
+#define RANGE_1A          2
+#define RANGE_100mA   3
+#define RANGE_10mA     4
+#define RANGE_1mA       5
+#define STP_RNG_OPT    AUTO
+#define STP_RNG_OPT2  1mA
+#define STP_RNG_OPT3  10mA
+#define STP_RNG_OPT4  100mA
+#define STP_RNG_OPT5  1A
+#define STP_AVG_OPT     1
+#define STP_AVG_OPT2   4
+#define STP_AVG_OPT3   8
+#define STP_AVG_OPT4   16
+#define STP_AVG_OPT5    32
 
-void setup() 
+ADS1115_WE adc = ADS1115_WE(ADC_ADDRESS);
+LiquidCrystal_I2C screen(LCD_ADDRESS, LCD_WIDTH, LCD_HEIGHT);
+
+// these two buffers are used for refreshing the LCD
+char Line0[LCD_WIDTH]; 
+char Line1[LCD_WIDTH];
+
+// these 2 dimensional arrays are for the predefined menu
+char start_menu[2][LCD_WIDTH ]={" SETUP",
+                                                      " MEASURE"};
+char setup_menu[2][LCD_WIDTH]={" Range: ",
+                                                         " Average: "};
+char meas_menu[2][LCD_WIDTH]={"I=100mA  Avg=32",
+                                                         " 100mOhms"};
+
+uint8_t symbol[8] = {0x00, 0x04, 0x06, 0x1F, 0x06, 0x04, 0x00}; //Arrow symbol
+uint8_t isymbol[8] = {0x1F, 0x1B, 0X19, 0X00, 0X19, 0X1B, 0X1F, 0X1F};// inverted arrow symbol
+
+uint8_t arrow_place=0;
+
+/**
+ * @brief displays or hides an arrow charachter on a specified screen cell
+ * @param int row
+ * @param int column
+ * @param bool visible
+ * @param int type (0 for normal, 1 for inverted)
+ */
+void arrow (int row, int col, bool show_hide, int type)
 {
-    Serial.begin(115200);
-    screen.init();
-    screen.createChar(0, symbol);
-    screen.createChar(1, isymbol);
-  
-    clearLcdLine(Line0,LCD_WIDTH);
-    clearLcdLine(Line1,LCD_WIDTH);
-  
-    LcdUpdate(Line0,Line1);
-    strncpy(&Line0[1],&menu[0][0],15);
-    strncpy(&Line1[1],&menu[1][0],15);
-    
-    pinMode(UP, INPUT);
-    pinMode(DOWN, INPUT);
-    pinMode(SELECT, INPUT);
+  if (show_hide == true)
+  {
+    screen.setCursor(row, col);
+    screen.write(type);
+  }
 }
 
-void loop() 
+/**
+ * @brief clears a line in an char array with specified lenght
+ * @param char[] line
+ * @param uint8_t len
+ */
+void clearLcdLine(char line[], uint8_t len)
 {
+    for(int i=0;i<len;i++) line[i]=0x020;
 
-    LcdUpdate(Line0,Line1);
-  
-    //line0[3]=0x41;
-    
-    strncpy(&Line0[1],&menu[option][0],15);
-    strncpy(&Line1[1],&menu[option+1][0],15);
-  
-  
-    
-  
-  
-    if (button(UP) == UP)
-    {
-      option -=1;
-      arrow_place-=1;
-    }
-  
-    if (button(DOWN) == DOWN)
-    {
-      option +=1;
-      arrow_place+=1;
-    }
-  
-  
-   if(option>=3) option=3;
-   if(option<=0) option=0;
+}
 
+/**
+ * @brief updates the lcd screen based on the strings provided
+ * @param char[] line1
+ * @param char[] line2
+ */
+ void LcdUpdate(char str0[],char str1[])
+{
+    screen.setCursor(0,0);
+    screen.print(str0);
+    screen.setCursor(0,1);
+    screen.print(str1);
+} 
+
+/**
+ * @brief Debounces an input and returns only when pressed
+ * @param input pin
+ * @return returns input pin only while pressed
+ */
+uint8_t button(int pin)
+{
+  #define KEY_ms      50
+  unsigned long keyPressedTime;
+  uint8_t c = 0;//the integer the will return the output
+  if ((digitalRead(pin) == HIGH) && (millis() - keyPressedTime) > KEY_ms)//debounce routine
+  {
+    keyPressedTime = millis();
+    c = pin;
+  }
+  return c;
+}
+
+/**
+ * @brief Reads the ADC and converts the reeding to millivolts
+ * @param ADS1115_MUX Channel
+ * @return float voltage in millivolts
+ */
+float readChannel(ADS1115_MUX channel) {
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  adc.startSingleMeasurement();
+  while(adc.isBusy()){delay(0);}
+  voltage = adc.getResult_mV();
+  return voltage;
+}
+
+/**
+ * @brief Avareges the ADC reading a specified amount up to 255
+ * @param uint8_t avarege amount
+ * @returns float avareged reading
+ */
+float avg(uint8_t avg_count)
+{
+  float temp=0;
+  for(uint8_t i=0; i<=avg_count; i++)
+  {
+    temp+=readChannel(ADS1115_COMP_0_GND);
+  }
+  return temp/avg_count;
+}
+
+/**
+ * @brief Calculates a mOhs value from a voltage reading on a set range
+ * @param float voltage reading
+ * @param uint8_t set range
+ * @returns float calculated mOhms value
+ */
+float calculate_mOhms(float reading, uint8_t range)
+{
+  float out=0;
+  switch (range)
+    case 2: //1A range
+      out = reading;
+      break;
+    case 3: //100mA range
+      out = reading/10;
+      break;
+    case 4: //10mA range
+      out = reading/100;
+      break;
+    case 3: //1mA range
+      out = reading/1000;
+      break;
+}
+
+/**
+ * @brief Initializes all the inputs and outputs. No Arguments
+ */
+void pins_init()
+{
+  pinMode(UP, INPUT);
+  pinMode(DOWN, INPUT);
+  pinMode(SELECT, INPUT);
+  pinMode(RANGE_1A, OUTPUT);
+  pinMode(RANGE_100mA, OUTPUT);
+  pinMode(RANGE_10mA, OUTPUT);
+  pinMode(RANGE_1mA, OUTPUT);
+}
+
+/**
+ * @brief Selects a current range
+ * @param uint8_t range
+*/
+void  range_select(uint8_t range)
+{
+  switch (range)
+  case 2: //1A range
+    digitalWrite(RANGE_1A, HIGH);
+    digitalWrite(RANGE_100mA, LOW);
+    digitalWrite(RANGE_10mA, LOW);
+    digitalWrite(RANGE_1mA, LOW);
+    break;
+  case 3: //100mA range
+    digitalWrite(RANGE_1A, LOW);
+    digitalWrite(RANGE_100mA, HIGH);
+    digitalWrite(RANGE_10mA, LOW);
+    digitalWrite(RANGE_1mA, LOW);
+    break;
+  case 4: //10mA range
+    digitalWrite(RANGE_1A, LOW);
+    digitalWrite(RANGE_100mA, LOW);
+    digitalWrite(RANGE_10mA, HIGH);
+    digitalWrite(RANGE_1mA, LOW);
+    break;
+  case 3: //1mA range
+   digitalWrite(RANGE_1A, LOW);
+    digitalWrite(RANGE_100mA, LOW);
+    digitalWrite(RANGE_10mA, LOW);
+    digitalWrite(RANGE_1mA, HIGH);
+    break;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+  screen.init();
+  screen.createChar(0, symbol);
+  screen.createChar(1, isymbol);
+
+  adc.setVoltageRange_mV(ADS1115_RANGE_6144);
+
+  clearLcdLine(Line0,LCD_WIDTH);
+  clearLcdLine(Line1,LCD_WIDTH);
+
+  LcdUpdate(Line0,Line1);
+  strncpy(&Line0[1],&start_menu[0][0],15);
+  strncpy(&Line1[1],&start_menu[1][0],15);
+}
+
+void loop()
+{
+  bool start = true;
+  bool setup_mode = false;
+  screen.backlight();
+  if(start)
+  {
+    strncpy(&Line0[1],&start_menu[0][0],15);
+    strncpy(&Line1[1],&start_menu[1][0],15);
+    arrow(0,arrow_place,true,0);
+  }
+  LcdUpdate(Line0,Line1);
 }
